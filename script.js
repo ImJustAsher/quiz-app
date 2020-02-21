0d2513d87f5524559f3223352910b44b88e6adf1
Asher <asher_3@hotmail.co.uk>
2020-02-21 19:16:47 +0000

    adding more questions

--- script.js
+++ script.js
@@ -1,18 +1,82 @@
 var quizController = (function() {
-	localStorage.setItem('data', JSON.stringify([ 1, 2, 3, 4 ]));
-	localStorage.setItem('data', JSON.stringify({ name: 'John' }));
-})();
+	function Question(id, questionText, options, correctAnswer) {
+		this.id = id;
+		this.questionText = questionText;
+		this.options = options;
+		this.correctAnswer = correctAnswer;
+	}
 
-var UIController = (function() {
-	var num1 = 30;
+	var questionLocalStorage = {
+		setQuestionCollection: function() {
+			localStorage.setItem('questionCollection', JSON.stringify(newCollection));
+		},
+		getQuestionCollection: function() {
+			return JSON.parse(localStorage.getItem('questionCollection'));
+		},
+		removeQuestionCollection: function() {
+			localStorage.removeItem('questionCollection');
+		}
+	};
 
 	return {
-		sum: function(num2) {
-			return num1 + num2;
+		addQuestionOnLocalStorage: function(newQuestText, opts) {
+			var optionsArr, corrAns, questionId, newQuestion;
+
+			optionsArr = [];
+
+			questionId = 0;
+
+			for (var i = 0; i < opts.length; i++) {
+				if (opts[i].value !== '') {
+					optionsArr.push(opts[i].value);
+				}
+				if (opts[i].previousElementSibling.checked && opts[i].value !== '') {
+					corrAns = opts[i].value;
+				}
+			}
+
+			if (questionLocalStorage.getQuestionCollection().length > 0) {
+				questionId =
+					questionLocalStorage.getQuestionCollection()[
+						questionLocalStorage.getQuestionCollection().length - 1
+					].id + 1;
+			} else {
+				questionId = 0;
+			}
+
+			newQuestion = new Question(questionId, newQuestText.value, optionsArr, correctAnswer);
+
+			console.log(newQuestion);
 		}
 	};
 })();
 
+/*******************************
+**********UI CONTROLLER*********
+*******************************/
+
+var UIController = (function() {
+	var domItems = {
+		// ****** Admin Panel Elements******
+		questInsertBtn: document.getElementById('question-insert-btn'),
+		newQuestionText: document.getElementById('new-question-text'),
+		adminOptions: document.querySelectorAll('.admin-option')
+	};
+
+	return {
+		getDomItems: domItems
+	};
+})();
+
+/*******************************
+***********CONTROLLER***********
+*******************************/
+//
+
 var controller = (function(quizCtrl, UICtrl) {
-	console.log(UICtrl.sum(100) + quizCtrl.publicMethod());
+	var selectedDomItems = UICtrl.getDomItems;
+
+	selectedDomItems.questInsertBtn.addEventListener('click', function() {
+		quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText, selectedDomItems.adminOptions);
+	});
 })(quizController, UIController);
